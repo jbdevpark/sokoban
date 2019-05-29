d83b403388f2328a95945a4cba54a3a3f951e065
jbdevpark <jbdevpark@gmail.com>
2019-05-29 14:49:57 +0900

    Lots of Changes + have to 'do something'

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 #define MAX 30
 #define STR_MAX 100
 #define STAGE_NUM 5
+#define MAX_LOG 1000
+
+#define UNDO_INIT 5
 
 #define TRUE 1
 #define FALSE 0
@@ -14,13 +17,13 @@
 //Print Location define
 
 #define HELLO_PRINT_X 10
-#define HELLO_PRINT_Y 1
+#define HELLO_PRINT_Y 2
 
 #define STATUS_PRINT_X 0
 #define STATUS_PRINT_Y (HELLO_PRINT_Y + 2)
 
 #define MAP_PRINT_X 0
-#define MAP_PRINT_Y (HELLO_PRINT_Y + 5)
+#define MAP_PRINT_Y (STATUS_PRINT_Y + 5)
 
 #define PLAYER_PRINT_X (MAP_PRINT_X + 2 * playerX)
 #define PLAYER_PRINT_Y (MAP_PRINT_Y + playerY)
@@ -32,19 +35,29 @@
 #define COMMAND_SCAN_Y (mapY + MAP_PRINT_Y + 1)
 
 #define DISHELP_PRINT_X 0
-#define DISHELP_PRINT_Y COMMAND_PRINT_Y + 2
+#define DISHELP_PRINT_Y (COMMAND_PRINT_Y + 2)
+
+#define PROMPT_PRINT_X 0
+#define PROMPT_PRINT_Y (COMMAND_PRINT_Y + 2)
 
 char name[STR_MAX];
 int record[STAGE_NUM];
+
+char log[STAGE_NUM][MAX_LOG];
+int logcnt = 0;
+
 int stage;
 int mapX, mapY;
 int playerX, playerY;
 char map[MAX][MAX];
 
+int remainUndo = UNDO_INIT;
+
 void nameInput(void);
 void mapMaker(int stage);
 int checkBoxStorage(void);
 
+void statPrint(void);
 int printNewStage(int stage);
 void printStage(int stage);
 
@@ -52,7 +65,10 @@ void mapPrinter(void);
 void playerFinder(void);
 
 void moveplayer(int xplus, int yplus);
-void move(int xplus, int yplus);
+int move(int xplus, int yplus);
+void loadMove(int repeat);
+
+int success(int stage);
 
 void gotoxy(int x, int y)
 {
@@ -65,6 +81,7 @@ void clear()
 
 int main(void)
 {
+	FILE* save;
 	clear();
 	printf("Start. . . .\n");
 	nameInput();
@@ -94,26 +111,43 @@ int main(void)
 				{
 					//left
 				case 'h':	case 'H':
-					move(-1, 0);
+					if (move(-1, 0))	log[stage - 1][record[stage - 1] - 1] = 'h';
 					break;
 					
 					//down
 				case 'j':	case 'J':
-					move(0, 1);
+					if (move(0, 1))	log[stage - 1][record[stage - 1] - 1] = 'j';
 					break;
 
 					//up
 				case 'k':	case 'K':
-					move(0, -1);
+					if (move(0, -1))	log[stage - 1][record[stage - 1] - 1] = 'k';
 					break;
 
 					//right
 				case 'l':	case 'L':
-					move(1, 0);
+					if (move(1, 0))	log[stage - 1][record[stage - 1] - 1] = 'l';
 					break;
 
 				case 'u':	case 'U':	//undo
-					//do something
+					if (remainUndo > 0 && record[stage - 1] > 0)
+					{
+						remainUndo--;
+						if (!printNewStage(stage))	return 0;
+						loadMove(record[stage - 1] - 1);
+					}
+					else if(remainUndo <= 0)
+					{
+						//prompt
+						gotoxy(PROMPT_PRINT_X, PROMPT_PRINT_Y);
+						printf("Undo 기회 모두사용함");
+					}
+					else if (record[stage - 1] <= 0)
+					{
+						gotoxy(PROMPT_PRINT_X, PROMPT_PRINT_Y);
+						printf("Nothing to Undo");
+					}
+
 					break;
 
 				case 'r':	case 'R':	//replay 현재 맵을 다시시작 (움직임 횟수 유지)
@@ -134,15 +168,64 @@ int main(void)
 
 				case 's':	case 'S':	//save 현재 상태 저장
 					//do something
+					save = fopen("sokoban.txt", "w");
+
+					//Line 1: NAME
+					//Line 2: Current Stage
+					//Line 3: Stage Records
+					//Line 4~STAGE_NUM + 4: Stage Logs
+					//Line STAGE_NUM + 5: remainUndo
+
+					fprintf(save, "%s", name);
+					fprintf(save, "\n");
+					fprintf(save, "%d\n", stage);
+					for (int i = 0; i < STAGE_NUM;i++)	fprintf(save, "%d ", record[i]);
+					fprintf(save, "\n");
+					for (int i = 0; i < STAGE_NUM; i++)
+					{
+						for (int j = 0; j < record[i]; j++)	fprintf(save, "%c", log[i][j]);
+						fprintf(save, "\n");
+					}
+					fprintf(save, "%d\n", remainUndo);
+
+					fclose(save);
+
 					if (command == 'e' || command == 'E')	return 0;
 					break;
 
 				case 'f':	case 'F':	//save파일 시점부터 게임진행
-					//do something
+
+					/////////////////////////////////////////////////
+					//BBBBBBBBBBBBBBBUUUUUUUUUUUUUUUUUGGGGGGGGGGGGGGG
+					/////////////////////////////////////////////////
+					
+					save = fopen("sokoban.txt", "r");
+
+					fgets(name,STR_MAX, save);
+					rewind(save);
+
+					fscanf(save, "%d", &stage);
+					rewind(save);
+
+					for (int i = 0; i < STAGE_NUM; i++)	fscanf(save, "%d", &record[i]);
+					rewind(save);
+
+					for (int i = 0; i < STAGE_NUM; i++)
+					{
+						for (int j = 0; j < record[i]; j++)	fgets(log[i], MAX_LOG, save);
+						rewind(save);
+					}
+					fscanf(save, "%d", &remainUndo);
+
+					printNewStage(stage);
+					loadMove(record[stage - 1]);
+
+					fclose(save);
+
 					break;
 
 				case 'd':	case 'D':	//display help
-					//do something
+					printStage(stage);
 					if (disHelp == FALSE)
 					{
 						disHelp = TRUE;
@@ -175,13 +258,13 @@ int main(void)
 					//do something
 					break;
 				}
-
+				statPrint();
 			}
 
 			//clear
 			gotoxy(COMMAND_SCAN_X, COMMAND_SCAN_Y);
 			for (int i = 0; i < strlen(commandArr); i++)	printf(" ");
-			
+			if (success(stage))	run = FALSE;
 		}
 		clear();
 	}
@@ -261,7 +344,15 @@ int checkBoxStorage(void)
 	if (cntBox != cntStorage)	return 0;
 	else return 1;
 }
-
+void statPrint(void)
+{
+	gotoxy(STATUS_PRINT_X, STATUS_PRINT_Y);
+	printf("%7s: %4d", "STAGE", stage);
+	gotoxy(STATUS_PRINT_X, STATUS_PRINT_Y + 1);
+	printf("%7s: % 4d", "UNDO", remainUndo);
+	gotoxy(STATUS_PRINT_X, STATUS_PRINT_Y + 2);
+	printf("%7s: % 4d", "RECORD", record[stage - 1]);
+}
 int printNewStage(int stage)
 {
 	clear();
@@ -282,6 +373,7 @@ int printNewStage(int stage)
 
 	gotoxy(COMMAND_PRINT_X, COMMAND_PRINT_Y);
 	printf("(Command) ");
+	statPrint();
 
 	return 1;
 }
@@ -297,6 +389,8 @@ void printStage(int stage)
 
 	gotoxy(COMMAND_PRINT_X, COMMAND_PRINT_Y);
 	printf("(Command) ");
+
+	statPrint();
 }
 void mapPrinter(void)
 {
@@ -336,14 +430,18 @@ void moveplayer(int xplus, int yplus)
 	playerX += xplus;
 	playerY += yplus;
 
+
+
 	gotoxy(PLAYER_PRINT_X, PLAYER_PRINT_Y);
 	printf("@");
 	map[playerY][playerX] = '@';
 
 	record[stage - 1]++;
 }
-void move(int xplus, int yplus)
+int move(int xplus, int yplus)
 {
+	int bef_playerX = playerX, bef_playerY = playerY;
+
 	if (playerX + xplus >= 0 && playerY + yplus >= 0 && playerX + xplus < mapX && playerY + yplus < mapY)
 	{
 		if (map[playerY + yplus][playerX + xplus] == '.')	moveplayer(xplus, yplus);
@@ -366,12 +464,54 @@ void move(int xplus, int yplus)
 			map[playerY + yplus][playerX + xplus] = '$';
 
 			//print O as remaining
+			//do something
 		}
 		else if (map[playerY + yplus][playerX + xplus] == 'O')
 		{
 			moveplayer(xplus, yplus);
 
 			//print O as remaining
+			//do something
 		}
 	}
+
+	if (bef_playerX != playerX || bef_playerY != playerY)	return 1;
+	else return 0;
+}
+void loadMove(int repeat)
+{
+	record[stage - 1] = 0;
+	for (int i = 0; i < repeat; i++)
+	{
+		switch (log[stage - 1][i])
+		{
+			//left
+		case 'h':	case 'H':
+			move(-1, 0);
+			break;
+
+			//down
+		case 'j':	case 'J':
+			move(0, 1);
+			break;
+
+			//up
+		case 'k':	case 'K':
+			move(0, -1);
+			break;
+
+			//right
+		case 'l':	case 'L':
+			move(1, 0);
+			break;
+		}
+	}
+}
+
+int success(int stage)
+{
+	//returns 1 when succeed
+	//else returns 0
+
+	//do something
 }
